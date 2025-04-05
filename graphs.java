00e4049b08c8088d52e78d09fa4fbf3bc912bd7d
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-04-05 22:31:36 +0530

    bfs

--- graphs.java
+++ graphs.java
@@ -148,3 +148,91 @@ class Solution {
         return topo;
     }
 }
+//shortest path in unweighted graph 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Collections;
+
+public class Solution 
+{
+    public static LinkedList<Integer> shortestPath(int[][] edges, int n, int m, int s, int t) 
+    {    
+        // We will store graph in an adjecency list.
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(n + 1);
+        
+        for (int i = 0; i <= n; i++) 
+        {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        // Making adjacency list ADJ from edges.
+        for (int i = 0; i < m; i++) 
+        {
+            int x, y;
+            x = edges[i][0];
+            y = edges[i][1];
+            adj.get(x).add(y);
+            adj.get(y).add(x);
+        }
+
+        /* 
+        	Declaring visited array and parent array , visited will be used in dfs.
+        	And parent will be use to recreate the path.
+        */
+        int visited[] = new int[n + 1];
+        int parent[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            visited[i] = 0;
+            parent[i] = -1;
+        }
+
+        /* 
+        	Starting BFS from node S.
+        	Q is the queue used in bfs.
+        */
+        LinkedList<Integer> q = new LinkedList<Integer>();
+        visited[s] = 1;
+        parent[s] = -1;
+        q.add(s);
+        
+        while (!q.isEmpty()) 
+        {
+            // Selecting a node and traversing all its neighbours.
+            int currentNode = q.remove();
+            
+            for (int i = 0; i < adj.get(currentNode).size(); i++) 
+            {
+                // If the node is not already visited we will add it to the Q.
+                if (visited[adj.get(currentNode).get(i)] == 0) 
+                {
+                    visited[adj.get(currentNode).get(i)] = 1;
+                    q.add(adj.get(currentNode).get(i));
+                    parent[adj.get(currentNode).get(i)] = currentNode;
+                }
+            }
+        }
+
+        // Now we will backtrack and recreate the path from S to T using parent array.
+        LinkedList<Integer> path = new LinkedList<Integer>();
+        int currentNode = t;
+        
+        // We will start from T and try to go back from here until we reach S.
+        path.add(currentNode);
+        
+        while (parent[currentNode] != -1) 
+        {    
+            /* 
+            	From current node we will find a neighbour who has distance equal to
+            	distance of current node - 1 , that node will be the parent of currentNode.
+            */
+            currentNode = parent[currentNode];
+            path.add(currentNode);
+        }
+
+        // We got path in from T to S , so we will reverse it and return it.
+        Collections.reverse(path);
+        return path;
+    }
+
+}
