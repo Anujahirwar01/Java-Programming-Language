3bc8f0bf62493726f7db9bbd5c7e2d8dbf27663e
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-04-12 23:23:16 +0530

    dsu problems

--- graphs.java
+++ graphs.java
@@ -459,3 +459,118 @@ class Solution {
         }
     }
 }
+//dsu problems 
+class Solution {
+    public class DisjointSet {
+    int parent[];
+    int size[];
+    DisjointSet(int nodes){ //0 based (5) 0 to 4
+        this.parent = new int[nodes];
+        this.size = new int[nodes];
+        for(int i=0;i<nodes;i++){
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+    public int findRootParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        parent[node] = findRootParent(parent[node]);
+        return parent[node];
+    }
+    public boolean unionBySize(int node1, int node2){
+        //1. find the root parent
+        int rootParent1 = findRootParent(node1);
+        int rootParent2 = findRootParent(node2);
+        if(rootParent1==rootParent2){
+            return false;
+        }
+        // 2, union of components
+        if(size[rootParent1]<size[rootParent2]){
+            parent[rootParent1] = rootParent2;
+            size[rootParent2] += size[rootParent1];
+        }else {
+            parent[rootParent2] = rootParent1;
+            size[rootParent1] += size[rootParent2];
+        }
+        return true;
+    }
+    }
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length < n-1){
+            return -1;
+        }
+        DisjointSet set = new DisjointSet(n);
+        int wire = 0;
+        for(int connection[] : connections){
+            int u = connection[0];
+            int v = connection[1];
+            if(set.unionBySize(u,v)){
+                wire++;
+            }
+        }
+        return n-1-wire;
+    }
+    
+}
+//another one
+class Solution {
+    public class DisjointSet {
+    int parent[];
+    int size[];
+    DisjointSet(int nodes){ //0 based (5) 0 to 4
+        this.parent = new int[nodes];
+        this.size = new int[nodes];
+        for(int i=0;i<nodes;i++){
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+    
+    public int findRootParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        parent[node] = findRootParent(parent[node]);
+        return parent[node];
+    }
+    public void unionBySize(int node1, int node2){
+        //1. find the root parent
+        int rootParent1 = findRootParent(node1);
+        int rootParent2 = findRootParent(node2);
+        if(rootParent1==rootParent2){
+            return;
+        }
+        // 2, union of components
+        if(size[rootParent1]<size[rootParent2]){
+            parent[rootParent1] = rootParent2;
+            size[rootParent2] += size[rootParent1];
+        }else {
+            parent[rootParent2] = rootParent1;
+            size[rootParent1] += size[rootParent2];
+        }
+    }
+    }
+    public boolean equationsPossible(String[] equations) {
+        DisjointSet dsu = new DisjointSet(26);
+        ArrayList<int[]> list = new ArrayList<>();
+        for(String equation : equations){
+            int u = equation.charAt(0) - 'a';
+            int v = equation.charAt(3) - 'a';
+            if(equation.charAt(1) == '='){
+                dsu.unionBySize(u,v);
+            }else{
+                list.add(new int[]{u,v});
+            }
+        }
+        for(int edge[] : list){
+            int u = edge[0];
+            int v = edge[1];
+            if(dsu.findRootParent(u) == dsu.findRootParent(v)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
