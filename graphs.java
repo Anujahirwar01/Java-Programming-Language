c9756ead5ad6721b463ed58f310887bc87cf6dfc
Anujahirwar01 <anujahirwar77010@gmail.com>
2025-04-15 15:04:31 +0530

    MST

--- graphs.java
+++ graphs.java
@@ -667,3 +667,107 @@ public class Solution {
         return count;
     }
 }
+//minimum spanning tree
+class Solution {
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        // Code Here.
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(int pair1[] , int pair2[]){
+                return pair1[2] - pair2[2];
+            }
+        });
+        int sum = 0;
+        ArrayList<int[]> edges = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        pq.offer(new int[]{-1,0,0});
+        while(!pq.isEmpty()){
+            int object[] = pq.poll();
+            int parent = object[0];
+            int node = object[1];
+            int weight = object[2];
+            if(vis[node]) continue;
+            vis[node] = true;
+            if(parent != -1){
+                edges.add(new int[]{parent,node});
+                sum += weight;
+            }
+            for(int neighbour[] : adj.get(node)){
+                int neighbourNode = neighbour[0];
+                int neighbourWeight = neighbour[1];
+                if(!vis[neighbourNode]){
+                    pq.offer(new int[]{node,neighbourNode,neighbourWeight});
+                }
+            }
+        }
+        return sum;
+    }
+}
+//kruskal
+class Solution {
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        // Code Here.
+        //(u, v, weight)
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(int pair1[], int pair2[]){
+                return pair1[2] - pair2[2];
+            }
+        });
+        // insert edges in pq
+        for(int i=0;i<V;i++){
+            for(int edges[] : adj.get(i)){
+                pq.offer(new int[]{i,edges[0],edges[1]});
+            }
+        }
+        DisjointSet dsu = new DisjointSet(V);
+        int sum=0;
+        while(!pq.isEmpty()){
+            int object[] = pq.poll();
+            int u = object[0];
+            int v = object[1];
+            int weight = object[2];
+            if(dsu.unionBySize(u,v)){
+                sum += weight;
+            }
+        }
+        return sum;
+    }
+}
+
+
+class DisjointSet {
+    int parent[];
+    int size[];
+    DisjointSet(int nodes){
+        this.parent = new int[nodes];
+        this.size = new int[nodes];
+        for(int i=0;i<nodes;i++){
+            this.parent[i] = i;
+            this.size[i] = 1;
+        }
+    }
+
+    public int findRootParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        parent[node] = findRootParent(parent[node]);
+        return parent[node];
+    }
+    public boolean unionBySize(int node1, int node2){
+        //1. find the root parent
+        int rootParent1 = findRootParent(node1);
+        int rootParent2 = findRootParent(node2);
+        if(rootParent1==rootParent2){
+            return false;
+        }
+        // 2, union of components
+        if(size[rootParent1]<size[rootParent2]){
+            parent[rootParent1] = rootParent2;
+            size[rootParent2] += size[rootParent1];
+        }else {
+            parent[rootParent2] = rootParent1;
+            size[rootParent1] += size[rootParent2];
+        }
+        return true;
+    }
+}
