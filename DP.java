0b966f4bb7195896935023cac00da4082687c48e
Anujahirwar01 <anujahirwar77010@gmail.com>
2026-01-08 19:21:14 +0530

    dp questions

--- DP.java
+++ DP.java
@@ -132,3 +132,62 @@ class Solution {
         return dp[m][n];
     }
 }
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for(int i=0; i<m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j=0; j<n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for(int i=1; i<m; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int j=1; j<n; j++){
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for(int i=2; i<=n; i++){
+            int oneDigit = Integer.parseInt(s.substring(i-1,i));
+            int twoDigit = Integer.parseInt(s.substring(i-2,i));
+            if(oneDigit >=1 && oneDigit <=9){
+                dp[i] += dp[i-1];
+            }
+            if(twoDigit >=10 && twoDigit <=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
